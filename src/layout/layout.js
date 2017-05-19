b95cdecfbeeb6f4ea1bb7cbf7b8d37a3da0f6b74
Antonio Vieiro <antonio@vieiro.net>
2017-05-19 20:06:08 +0200

    Breakpoints improved

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -40,7 +40,7 @@
    * @private
    */
   MaterialLayout.prototype.Constant_ = {
-    MAX_WIDTH: '(max-width: 1024px)',
+    MAX_WIDTH: '(max-width: 1023px)',
     TAB_SCROLL_PIXELS: 100,
     RESIZE_TIMEOUT: 100,
 
